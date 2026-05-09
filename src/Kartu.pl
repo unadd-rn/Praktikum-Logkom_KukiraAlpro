b8b311d97ad514f726cb3fd53bbc888af974185a
Edbertf <edbertfernando09@gmail.com>
2026-05-09 15:44:24 +0700

    feat:representasi kartu

---
+++ src/Kartu.pl
@@ -0,0 +1,69 @@
+/*kartu angka*/
+kartu(merah,0).
+kartu(merah,1).
+kartu(merah,2).
+kartu(merah,3).
+kartu(merah,4).
+kartu(merah,5).
+kartu(merah,6).
+kartu(merah,7).
+kartu(merah,8).
+kartu(merah,9).
+
+kartu(kuning,0).
+kartu(kuning,1).
+kartu(kuning,2).
+kartu(kuning,3).
+kartu(kuning,4).
+kartu(kuning,5).
+kartu(kuning,6).
+kartu(kuning,7).
+kartu(kuning,8).
+kartu(kuning,9).
+
+kartu(hijau,0).
+kartu(hijau,1).
+kartu(hijau,2).
+kartu(hijau,3).
+kartu(hijau,4).
+kartu(hijau,5).
+kartu(hijau,6).
+kartu(hijau,7).
+kartu(hijau,8).
+kartu(hijau,9).
+
+kartu(biru,0).
+kartu(biru,1).
+kartu(biru,2).
+kartu(biru,3).
+kartu(biru,4).
+kartu(biru,5).
+kartu(biru,6).
+kartu(biru,7).
+kartu(biru,8).
+kartu(biru,9).
+
+/*kartu skip*/
+kartu(merah,skip).
+kartu(kuning,skip).
+kartu(hijau,skip).
+kartu(biru,skip).
+
+/*kartu reverse*/
+kartu(merah,reverse).
+kartu(kuning,reverse).
+kartu(hijau,reverse).
+kartu(biru,reverse).
+
+/*kartu draw two*/
+kartu(merah,drawTwo).
+kartu(kuning,drawTwo).
+kartu(hijau,drawTwo).
+kartu(biru,drawTwo).
+
+/*kartu wild*/
+kartu(hitam,wild).
+
+/*kartu wild draw four*/
+kartu(hitam,wildDrawFour).
+
