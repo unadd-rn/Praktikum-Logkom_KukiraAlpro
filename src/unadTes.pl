d3d60aee95c042ac3506abed28a72213e914538c
unadd-rn <auliaunad@gmail.com>
2026-05-11 00:32:03 +0700

    refactor/tadi-males-ngetik-lengkap-varnya

--- src/unadTes.pl
+++ src/unadTes.pl
@@ -1,32 +1,18 @@
 % ini biar jalan aja
 giliran(alice).
-<<<<<<< Updated upstream
-namaP1(alice).
-namaP2(bob).
-kartuP1([]).
-kartuP2([]).
-=======
 namaPemain1(alice).
 namaPemain2(bob).
 kartuPemain1([]).
 kartuPemain2([]).
->>>>>>> Stashed changes
 discard_top(kartu(merah, 5)).
 warnaAktif(merah).
 urutanPemain([alice, bob]).
 jumlahPemain(2).
 
-<<<<<<< Updated upstream
-namaP3(none).
-namaP4(none).
-kartuP3([]).
-kartuP4([]).
-=======
 namaPemain3(none).
 namaPemain4(none).
 kartuPemain3([]).
 kartuPemain4([]).
->>>>>>> Stashed changes
 
 lihatCommand :-
     writeln('!!!COMMANDS!!!'),
@@ -54,17 +40,10 @@ cetak([kartu(W,J)|T], N) :-
     N1 is N + 1,
     cetak(T, N1).
 
-<<<<<<< Updated upstream
-milik(N, K) :- namaP1(N), kartuP1(K), !.
-milik(N, K) :- namaP2(N), kartuP2(K), !.
-milik(N, K) :- namaP3(N), kartuP3(K), !.
-milik(N, K) :- namaP4(N), kartuP4(K), !.
-=======
 milik(N, K) :- namaPemain1(N), kartuPemain1(K), !.
 milik(N, K) :- namaPemain2(N), kartuPemain2(K), !.
 milik(N, K) :- namaPemain3(N), kartuPemain3(K), !.
 milik(N, K) :- namaPemain4(N), kartuPemain4(K), !.
->>>>>>> Stashed changes
 
 cekInfo :-
     discard_top(Atas),
@@ -90,14 +69,7 @@ infoP(I, N) :-
     I1 is I + 1,
     infoP(I1, N).
 
-<<<<<<< Updated upstream
-milikI(1, N, K) :- namaP1(N), kartuP1(K).
-milikI(2, N, K) :- namaP2(N), kartuP2(K).
-milikI(3, N, K) :- namaP3(N), kartuP3(K).
-milikI(4, N, K) :- namaP4(N), kartuP4(K).
-=======
 milikI(1, N, K) :- namaPemain1(N), kartuPemain1(K).
 milikI(2, N, K) :- namaPemain2(N), kartuPemain2(K).
 milikI(3, N, K) :- namaPemain3(N), kartuPemain3(K).
 milikI(4, N, K) :- namaPemain4(N), kartuPemain4(K).
->>>>>>> Stashed changes
