aa70a386adde8e62b7207c41d643005ee04c9262
Anugerah Kurniawan <angeldemons@Anugerahs-MacBook-Pro.local>
2026-05-12 18:05:53 +0700

    feat: integrasi setiap module (dengan penyesuaian di masing-masing file) dan membuat main.pl

--- src/Turn.pl
+++ src/Turn.pl
@@ -1,12 +1,4 @@
-:- include('Kartu.pl').
-:- dynamic(isStart/1). 
-:- dynamic(giliran/1).
-:- dynamic(kartuPemain/2).
-:- dynamic(topKartu/1).
-:- dynamic(urutanPemain/1).
-:- dynamic(deck/1).
 
-isStart(0).
 
 /* Predikat Umum */
 appendList([],H,H).
@@ -63,7 +55,7 @@ mainkanKartu(_) :-
 
 validasiTop(Kartu, Top) :-
     kartuValid(Kartu,Top),!.
-validasiTop(Kartu, Top) :-
+validasiTop(_Kartu,_Top) :-
     write('Kartu yang dimainkan tidak valid!'),
     fail.
 
@@ -76,14 +68,12 @@ ambilKartu :-
 ambilKartu :-
     isStart(1),
     giliran(Pemain),
-    kartuPemain(Pemain,List),
     deck(Deck),
-    Deck = [Kartu|Sisa],!,
+    kartuPemain(Pemain,List),
+    randomKartu(Deck, Kartu),
     appendList(List,[Kartu], List1),
     retract(kartuPemain(Pemain,_)),
     asserta(kartuPemain(Pemain,List1)),
-    retract(deck(_)),
-    asserta(deck(Sisa)),
     format('~w mendapatkan kartu: ~w.~n', [Pemain,Kartu]),
     nextTurn.
 ambilKartu :-
@@ -92,36 +82,6 @@ ambilKartu :-
     nextTurn,
     fail.
 
-
-
-/* Sampel 4 Pemain */
-startGame :-
-    isStart(0),
-    retractall(giliran(_)),
-    retractall(kartuPemain(_,_)),
-    retractall(topKartu(_)),
-    retractall(urutanPemain(_)),
-    retractall(deck(_)),
-    retractall(isStart(_)),
-
-    asserta(isStart(1)),
-    asserta(urutanPemain([fritz, prana, edbert, unad])),
-    asserta(giliran(fritz)),
-    asserta(topKartu(kartu(biru,6))),
-
-    asserta(kartuPemain(fritz, [kartu(merah,6), kartu(kuning,6), kartu(hijau,6)])),
-    asserta(kartuPemain(prana, [kartu(merah,7), kartu(kuning,8), kartu(hijau,9)])),
-    asserta(kartuPemain(edbert, [kartu(merah,1), kartu(kuning,2), kartu(hijau,3)])),
-    asserta(kartuPemain(unad, [kartu(merah,4), kartu(kuning,5), kartu(hijau,0)])),
-    asserta(deck([kartu(hitam,wildDrawFour), kartu(merah,skip), kartu(biru,drawTwo)])),
-
-    giliran(Pemain),
-    write('=========================================='),nl,
-    write(' Tes Turn 4 Pemain '),nl,
-    write('=========================================='),nl,
-    format('Giliran: ~w.~n', [Pemain]),
-    write('Silakan tentukan aksi Anda!'),nl,
-    !.
 
 exit :-
     isStart(1),!,
--- src/startGame.pl
+++ src/startGame.pl
@@ -1,89 +1,19 @@
-use_module(library(random)).
-
-/*kartu angka*/
-kartu(merah,0).
-kartu(merah,1).
-kartu(merah,2).
-kartu(merah,3).
-kartu(merah,4).
-kartu(merah,5).
-kartu(merah,6).
-kartu(merah,7).
-kartu(merah,8).
-kartu(merah,9).
-
-kartu(kuning,0).
-kartu(kuning,1).
-kartu(kuning,2).
-kartu(kuning,3).
-kartu(kuning,4).
-kartu(kuning,5).
-kartu(kuning,6).
-kartu(kuning,7).
-kartu(kuning,8).
-kartu(kuning,9).
-
-kartu(hijau,0).
-kartu(hijau,1).
-kartu(hijau,2).
-kartu(hijau,3).
-kartu(hijau,4).
-kartu(hijau,5).
-kartu(hijau,6).
-kartu(hijau,7).
-kartu(hijau,8).
-kartu(hijau,9).
-
-kartu(biru,0).
-kartu(biru,1).
-kartu(biru,2).
-kartu(biru,3).
-kartu(biru,4).
-kartu(biru,5).
-kartu(biru,6).
-kartu(biru,7).
-kartu(biru,8).
-kartu(biru,9).
-
-/*kartu skip*/
-kartu(merah,skip).
-kartu(kuning,skip).
-kartu(hijau,skip).
-kartu(biru,skip).
-
-/*kartu reverse*/
-kartu(merah,reverse).
-kartu(kuning,reverse).
-kartu(hijau,reverse).
-kartu(biru,reverse).
-
-/*kartu draw two*/
-kartu(merah,drawTwo).
-kartu(kuning,drawTwo).
-kartu(hijau,drawTwo).
-kartu(biru,drawTwo).
-
-/*kartu wild*/
-kartu(hitam,wild).
-
-/*kartu wild draw four*/
-kartu(hitam,wildDrawFour).
-
-:- dynamic(namaPemain/1).
-:- dynamic(jumlahPemain/1).
-:- dynamic(kartuDiscardTop/1).
-:- dynamic(suksesor/2).
-
 
 
 
 startGame :-
+    isStart(0),
     retractall(namaPemain(_)),
     retractall(jumlahPemain(_)),
-    retractall(kartuDiscardTop(_)),
+    retractall(topKartu(_)),
     retractall(suksesor(_,_)),
+    retractall(giliran(_)),
+    retractall(kartuPemain(_,_)),
+    retractall(urutanPemain(_)),
+    retractall(isStart(_)),
+    asserta(isStart(1)),
     inputJumlah(N),
-    inputNama(N, 1, NamaPemain),
+    inputNama(N, 1, _NamaPemain),
     findall(X, namaPemain(X), ListPemain),
     
     acakList(ListPemain, UrutanPemain),
@@ -91,9 +21,14 @@ startGame :-
     write('Urutan pemain: '),
     printList(UrutanPemain),!,
     faktaSuksesor(UrutanPemain), nl,
+    asserta(urutanPemain(UrutanPemain)),
     jalanPertama(A),
+    asserta(giliran(A)),
+    deck(Deck),
+    bagiKePemain(UrutanPemain,Deck),
     write('Setiap pemain mendapatkan 7 kartu acak.'), nl,
-    write('Kartu discard top: '), topCard, nl,
+    write('Kartu discard top: '),
+    topCard, nl,
     format('Giliran ~w.', [A]). 
 
     % random_card(ListPemain, Pemain, NewHand),
@@ -107,8 +42,6 @@ printList([H|T]):-
     write(' - '),
     printList(T).
     
-    
-
 
 inputJumlah(Jumlah):-
     write('Masukkan jumlah pemain: '), read(Cobajumlah),
@@ -185,22 +118,15 @@ kartuBantingan(_, Y):-
     Y \== drawTwo.
 
 topCard:-
-    findall(kartu(X, Y), kartu(X, Y), ListKartu), 
-    random_card(ListKartu, kartu(Warna,Angka), _),
+    deck(Deck),
+    randomKartu(Deck, kartu(Warna,Angka)),
     kartuBantingan(Warna, Angka), !,    
-    assertz(kartuDiscardTop(Warna, Angka)),
+    assertz(topKartu(kartu(Warna,Angka))),
     write(Warna), write('-'), write(Angka).
+topCard:-
+    topCard.
 
 jalanPertama(A):-
     suksesor(A,_),
     \+suksesor(_, A).
-    
-
-
-
-
-
-
-
-
     
